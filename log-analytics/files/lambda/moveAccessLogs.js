977435035ebab300e35de16b21813479929f0b05
Kazuya Miyagi <KazuyaMiyagi@users.noreply.github.com>
2023-08-03 19:52:57 +0900

    prettier -w files/lambda/moveAccessLogs.js

--- log-analytics/files/lambda/moveAccessLogs.js
+++ log-analytics/files/lambda/moveAccessLogs.js
@@ -1,9 +1,7 @@
 // Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
-const {
-  S3
-} = require("@aws-sdk/client-s3");
-const s3 = new S3({ apiVersion: '2006-03-01' });
+const { S3 } = require("@aws-sdk/client-s3");
+const s3 = new S3({ apiVersion: "2006-03-01" });
 
 // prefix to copy partitioned data to w/o leading but w/ trailing slash
 const targetKeyPrefix = process.env.TARGET_KEY_PREFIX;
@@ -11,48 +9,54 @@ const targetKeyPrefix = process.env.TARGET_KEY_PREFIX;
 // regex for filenames by Amazon CloudFront access logs. Groups:
 // - 1.	year
 // - 2.	month
-// - 3.	day 
+// - 3.	day
 // - 4.	hour
-const datePattern = '[^\\d](\\d{4})-(\\d{2})-(\\d{2})-(\\d{2})[^\\d]';
-const filenamePattern = '[^/]+$';
+const datePattern = "[^\\d](\\d{4})-(\\d{2})-(\\d{2})-(\\d{2})[^\\d]";
+const filenamePattern = "[^/]+$";
 
 exports.handler = async (event, context, callback) => {
-  const moves = event.Records.map(record => {
+  const moves = event.Records.map((record) => {
     const bucket = record.s3.bucket.name;
     const sourceKey = record.s3.object.key;
 
-    const sourceRegex = new RegExp(datePattern, 'g');
+    const sourceRegex = new RegExp(datePattern, "g");
     const match = sourceRegex.exec(sourceKey);
     if (match == null) {
-      console.log(`Object key ${sourceKey} does not look like an access log file, so it will not be moved.`);
+      console.log(
+        `Object key ${sourceKey} does not look like an access log file, so it will not be moved.`,
+      );
     } else {
       const [, year, month, day, hour] = match;
 
-      const filenameRegex = new RegExp(filenamePattern, 'g');
+      const filenameRegex = new RegExp(filenamePattern, "g");
       const filename = filenameRegex.exec(sourceKey)[0];
 
       const targetKey = `${targetKeyPrefix}year=${year}/month=${month}/day=${day}/hour=${hour}/${filename}`;
       console.log(`Copying ${sourceKey} to ${targetKey}.`);
 
       const copyParams = {
-        CopySource: bucket + '/' + sourceKey,
+        CopySource: bucket + "/" + sourceKey,
         Bucket: bucket,
-        Key: targetKey
+        Key: targetKey,
       };
       const copy = s3.copyObject(copyParams);
 
       const deleteParams = { Bucket: bucket, Key: sourceKey };
 
-      return copy.then(function () {
-        console.log(`Copied. Now deleting ${sourceKey}.`);
-        const del = s3.deleteObject(deleteParams);
-        console.log(`Deleted ${sourceKey}.`);
-        return del;
-      }, function (reason) {
-        const error = new Error(`Error while copying ${sourceKey}: ${reason}`);
-        callback(error);
-      });
-
+      return copy.then(
+        function () {
+          console.log(`Copied. Now deleting ${sourceKey}.`);
+          const del = s3.deleteObject(deleteParams);
+          console.log(`Deleted ${sourceKey}.`);
+          return del;
+        },
+        function (reason) {
+          const error = new Error(
+            `Error while copying ${sourceKey}: ${reason}`,
+          );
+          callback(error);
+        },
+      );
     }
   });
   await Promise.all(moves);
