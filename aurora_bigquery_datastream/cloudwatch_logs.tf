506ee8c7dd54979338aec4dc8ab639be84d247a3
Kazuya Miyagi <KazuyaMiyagi@users.noreply.github.com>
2024-02-08 13:19:32 +0900

    新規作成時に CloudWatch Logs のリソースが Cycle Error になるのでコメントを追加

--- aurora_bigquery_datastream/cloudwatch_logs.tf
+++ aurora_bigquery_datastream/cloudwatch_logs.tf
@@ -1,5 +1,6 @@
 resource "aws_cloudwatch_log_group" "fis" {
   name              = "/aws/fis"
   retention_in_days = 14
-  kms_key_id        = aws_kms_key.cloudwatch_logs.arn
+  # 新規作成時は一度コメントアウトして apply する
+  kms_key_id = aws_kms_key.cloudwatch_logs.arn
 }
